1ae7119f3d70ec892bcd1847f5b38eabc9fb9ea5
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2024-06-17 19:19:15 +0200

    respond with 200 ok

--- app/server.go
+++ app/server.go
@@ -7,18 +7,21 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	fmt.Println("HTTP server listening on port 4221")
+
+	defer l.Close()
+
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
